ddca693abe89212cd7374ddd68cbe426f5a802e5
James Holdren <jamesdholdren@gmail.com>
2023-04-16 23:24:57 -0400

    Can holepunch with seeds

--- src/diver.rs
+++ src/diver.rs
@@ -1,16 +1,32 @@
+use anyhow::Result;
+use clap::{arg, Command};
 use std::net::SocketAddr;
 
-use diver::PotentialClient;
+use diver::holepunch;
+
+fn cli() -> Command {
+    Command::new("diver")
+        .about("The main diver client")
+        .subcommand_required(true)
+        .subcommand(Command::new("send").about("Sends a file"))
+        .subcommand(Command::new("recv").about("Receives a file"))
+}
 
 #[tokio::main]
-async fn main() {
-    let mut pc = PotentialClient::new()
-        .await
-        .expect("error making potential client");
+async fn main() -> Result<()> {
+    let matches = cli().get_matches();
 
-    println!("Listening on {}", pc.port);
+    match matches.subcommand() {
+        Some(("send", submatches)) => {
+            let remote: SocketAddr = "127.0.0.1:4444".parse().expect("error parsing remote");
+            // TODO: Open the file, start sending over wireguard
+            // Hole punch it up
+            holepunch(remote).await?;
+        }
+        _ => unreachable!(),
+    };
 
-    let remote: SocketAddr = "127.0.0.1:4444".parse().expect("error parsing remote");
-
-    pc.holepunch(remote).await.expect("error punching a hole");
+    Ok(())
 }
+
+// TODO: Make a function that generates a private/public key pair
--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,105 @@
-use rand::Rng;
-use std::net::{SocketAddr, TcpListener};
+use anyhow::{anyhow, Result};
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::str;
 use tokio::net::UdpSocket;
 use tokio::time;
 
 const HOLE_PUNCH_MAX_TRIES: u16 = 10;
 
-// Representing a connection that we haven't punched yet
-pub struct PotentialClient {
-    pub port: u16,
-    sock: UdpSocket,
+pub async fn holepunch(remote_addr: SocketAddr) -> Result<UdpSocket> {
+    // The '0' port randomly assigns an unused port
+    let sock = UdpSocket::bind(("127.0.0.1", 0)).await?;
+    perform_holepunch(sock, remote_addr).await
 }
 
-#[derive(Debug)]
-pub enum PotentialClientError {
-    NoFreePort,
-    BindErr(std::io::Error),
-}
+// Performs the holepunch and returns a socket that has successfully made it out
+async fn perform_holepunch(sock: UdpSocket, remote_addr: SocketAddr) -> Result<UdpSocket> {
+    println!("We're listening on: {}", sock.local_addr()?.port());
 
-impl PotentialClient {
-    pub async fn new() -> Result<Self, PotentialClientError> {
-        // Get a free port
-        let Some(port) = freeport() else {
-            return Err(PotentialClientError::NoFreePort);
-        };
+    let mut rng = StdRng::from_entropy();
+    // Generate some number to be our seed to share with the remote
+    let seed = rng.gen_range(1..4444);
+    // We initially don't know the remote's seed
+    let mut known_seed: Option<u16> = None;
 
-        // Get us a UDP socket
-        let sock = match UdpSocket::bind(("127.0.0.1", port)).await {
-            Ok(sock) => sock,
-            Err(err) => return Err(PotentialClientError::BindErr(err)),
-        };
+    let mut buf = [0; 1024];
 
-        Ok(PotentialClient { port, sock })
-    }
+    for _i in 0..HOLE_PUNCH_MAX_TRIES {
+        // Start with a send
+        let msg = HolePunchMessage { seed, known_seed };
+        let marshalled = serde_json::to_string(&msg)?;
+        sock.send_to(marshalled.as_bytes(), remote_addr).await?;
 
-    // Performs the holepunch and tries to upgrade the potential client to a real one
-    pub async fn holepunch(
-        &mut self,
-        remote_addr: SocketAddr,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let mut rng = rand::thread_rng();
-
-        let mut buf = [0; 1024];
-        for _i in 0..HOLE_PUNCH_MAX_TRIES {
-            // Start with a send
-            self.sock.send_to(&buf, remote_addr).await?;
-
-            // Then wait to receive with some jitter
-            if time::timeout(
-                time::Duration::from_millis(1000 + rng.gen_range(0..500)),
-                self.sock.recv(&mut buf),
-            )
-            .await
-            .is_ok()
-            {
-                println!("Got a response!");
-                return Ok(());
+        // Then wait to receive with some jitter
+        if let Ok(Ok(msg_size)) = time::timeout(
+            time::Duration::from_millis(1000 + rng.gen_range(0..100)),
+            sock.recv(&mut buf),
+        )
+        .await
+        {
+            // TODO: Clear the buffer
+            println!("Buffer: {:?}", buf);
+            // We get a response parse it out
+            let msg: HolePunchMessage = match serde_json::from_slice(&buf[0..msg_size]) {
+                Ok(msg) => msg,
+                Err(err) => {
+                    println!("error deserialzing: {}", err);
+                    continue;
+                }
             };
-            println!("Trying again...");
-        }
+            println!("Got message: {:?}", msg);
+            // If we don't have a known seed, set it and continue
+            if known_seed.is_none() {
+                known_seed = Some(msg.seed);
+                continue;
+            }
+            // If we do have a seed, make sure it matches
+            if known_seed.unwrap() != msg.seed {
+                return Err(anyhow!("seeds didn't match"));
+            }
+            // Check if the remote knows our seed
+            if msg.known_seed.unwrap_or(0) == seed {
+                // The remote knows us, we're done!
+                return Ok(sock);
+            }
+        };
 
-        Ok(())
+        println!("Trying again...");
     }
+
+    Err(anyhow!("too many tries holepunching"))
 }
 
-// Finds a free port for us to use
-fn freeport() -> Option<u16> {
-    // Loop through
-    (49152..65535).find(|&possible| TcpListener::bind(("127.0.0.1", possible)).is_ok())
+// Represents what each client sends during the holepunch
+#[derive(Serialize, Deserialize, Debug)]
+struct HolePunchMessage {
+    seed: u16,               // The client's seed that it's sending
+    known_seed: Option<u16>, // If the client knows the other's secret
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
-    #[test]
-    fn freeport_gets_next_port() {
-        // Find a free port, then take it, and we should have the next one?
-        let port = freeport().expect("error finding the first open port");
-        let _listener = TcpListener::bind(("127.0.0.1", port)).expect("error binding to free port");
+    #[tokio::test]
+    async fn holepunch_connects() {
+        // The addresses for each client
+        let addr_1: SocketAddr = "127.0.0.1:4444".parse().unwrap();
+        let addr_2: SocketAddr = "127.0.0.1:4445".parse().unwrap();
+        let sock_1 = UdpSocket::bind(addr_1).await.unwrap();
+        let sock_2 = UdpSocket::bind(addr_2).await.unwrap();
 
-        assert_eq!(port + 1, freeport().unwrap());
+        let handle_1 = tokio::spawn(async move {
+            perform_holepunch(sock_1, addr_2).await.unwrap();
+        });
+        let handle_2 = tokio::spawn(async move {
+            perform_holepunch(sock_2, addr_1).await.unwrap();
+        });
+
+        // Wait for both
+        tokio::join!(handle_1, handle_2).0.unwrap();
     }
-
-    #[test]
-    fn holepunch() {}
 }
